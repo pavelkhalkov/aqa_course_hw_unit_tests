ea4959dd38916a33dabd56038211761dcfd741a2
pkhalkov <pavel.khalkov@mail.ru>
2025-02-15 21:01:58 +0300

    Completed HW-4

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -3,6 +3,23 @@
 (НЕ ОБЯЗАТЕЛЬНО)
 
 Преобразовать Task 2 таким образом, чтобы значение НАПРИМЕР '2' (т.е. ЛЮБАЯ строка в которой лежат ТОЛЬКО ЦИФРЫ) пропускалось, 
-  преобразовываясь в number
+преобразовываясь в number
 
 */
+
+let minAge = 18;
+let maxAge = 60;
+let ageInput = '2';
+let age = Number(ageInput);
+
+if (isNaN(age)) {
+  console.log("Incorrect data type");
+} else if (age < minAge) {
+  console.log(`You don't have access cause your age is ${age}. It's less then ${minAge}`);
+} else if (age >= minAge && age < maxAge) {
+  console.log("Welcome!");
+} else if (age > maxAge) {
+  console.log("Keep calm and look Culture channel");
+} else {
+  console.log('Technical work');
+}
