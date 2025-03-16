257528482b5cd9724c873c6f89d0e5a17f3ee7f7
pkhalkov <pavel.khalkov@mail.ru>
2025-03-16 23:52:29 +0300

    Completed HW-10

--- homework-solutions/js/hw-10/task-1.mjs
+++ homework-solutions/js/hw-10/task-1.mjs
@@ -5,7 +5,14 @@
     Значения в строке должны ссылаться на контекст ЭТОГО ОБЪЕКТА, без подмен.
 */
 
-let qa;
+let qa = {
+  name: "Semen",
+  age: 19,
+  salary: 1900,
+  getInfo(greetingsWord) {
+      return `${greetingsWord}, my name is ${this.name}, I'm ${this.age} and my salary is ${this.salary}`
+  }
+};
 
 /*
  2. Changing the context
@@ -15,32 +22,46 @@ let qa;
   - Вызовите метод getInfo объекта qa c контекстом вызова объекта anotherQa с помощью метода apply()
  */
 
-let anotherQa;
+let anotherQa = {
+    name: "Dmitry",
+    age: 35,
+    salary: 3000,
+};
 
 // Используйте bind с greetingWord "Hello"
-let bindResult;
+let bindResult = qa.getInfo.bind(anotherQa)('Hello');
+//const result = bindResult('Hello')
+console.log(bindResult)
 
 // Используйте call с greetingWord "Hi"
-let callResult;
+let callResult = qa.getInfo.call(anotherQa, 'Hi');
+console.log(callResult)
 
 // Используйте apply с greetingWord "Hey"
-let applyResult;
-
-/*
- 3. Closures
-  - Создайте функцию createCounter(),
-  - Создайте в функции createCounter переменную count, которая будет равна 0
-  - Верните из функции createCounter новую функцию
-  - В теле новой функции реализуйте увеличение count на + 1 при каждом вызове функции
-  - После увеличение каунтера выводите в консоль Function was called ${count} times
-  - Создайте переменную functionCallCounter, в которой будет лежать результат createCounter()
-  - Вызовите functionCallCounter() 5 раз, убедитесь что в консоли верно выводятся данные
-*/
+let applyResult = qa.getInfo.apply(anotherQa, ['Hey']);
+console.log(applyResult)
+// /*
+//  3. Closures
+//   - Создайте функцию createCounter(),
+//   - Создайте в функции createCounter переменную count, которая будет равна 0
+//   - Верните из функции createCounter новую функцию
+//   - В теле новой функции реализуйте увеличение count на + 1 при каждом вызове функции
+//   - После увеличение каунтера выводите в консоль Function was called ${count} times
+//   - Создайте переменную functionCallCounter, в которой будет лежать результат createCounter()
+//   - Вызовите functionCallCounter() 5 раз, убедитесь что в консоли верно выводятся данные
+// */
 
 function createCounter() {
-  // Ваш код
-}
+  let count = 0 
+ 
+  return function() {
+     count++;
+     console.log(`Function was called ${count} times`);
+     return count;
+  }
+ }
 
-const functionCallCounter = createCounter();
+const functionCallCounter = createCounter(); 
+console.log(functionCallCounter)
 
 export { qa, bindResult, callResult, applyResult, functionCallCounter, anotherQa };
--- homework-solutions/js/hw-10/task-2.mjs
+++ homework-solutions/js/hw-10/task-2.mjs
@@ -7,8 +7,18 @@
   Ожидается: { 1: 1, 2: 2, 3: 1, 4: 3, 5: 1 }
 */
 
+const numbers = [1, 2, 2, 3, 4, 4, 4, 5];
+
 function countOccurrences(arr) {
-  // ваш код
+  const count = {};
+
+  for (let num of arr) {
+    count[num] = (count[num] || 0) + 1 
+  }
+return count 
 }
+
+console.log(countOccurrences(numbers))
+
 
 export { countOccurrences };
