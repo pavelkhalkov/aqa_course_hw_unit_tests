83b99cd9cb398e6439606b40c38fe41c43186424
pkhalkov <pavel.khalkov@mail.ru>
2025-08-05 23:49:47 +0400

    Completed HW-5

--- homework-solutions/js/hw-5/task-1.js
+++ homework-solutions/js/hw-5/task-1.js
@@ -6,6 +6,16 @@
 
 let evenNumbersResult = '';
 
+for (let i = 10; i>=0; i-=2) {
+ if(evenNumbersResult === '') {
+     evenNumbersResult += i;
+ } else {
+    evenNumbersResult += '-' + i;
+ }
+}
+
+console.log(evenNumbersResult)
+
 /**
  * Создать строку из 5 строк с увеличивающимся количеством смайликов ":)".
  * Переменная для результата `smilePatternResult` уже создана и содержит пустую строку.
@@ -19,12 +29,22 @@ let evenNumbersResult = '';
 
 let smilePatternResult = '';
 
+for (let i = 1; i <= 5; i++) {
+    if(i > 1) {
+        smilePatternResult += '\n'
+    }
+    smilePatternResult += ':)'.repeat(i)
+}
+
+console.log(smilePatternResult)
+
 /**
  * Заменить все пробелы в переменной text на "1".
  * Переменная для результата `replaceSpacesWithOneResult` уже создана и содержит пустую строку.
  * Ожидаемый результат: 'Hello!1I1am1a1JS1student!'
  */
 const text = 'Hello! I am a JS student!';
-let replaceSpacesWithOneResult = '';
+let replaceSpacesWithOneResult = text.split(' ').join('1');
+console.log(replaceSpacesWithOneResult)
 
 export { evenNumbersResult, smilePatternResult, replaceSpacesWithOneResult };
--- homework-solutions/js/hw-5/task-3.js
+++ homework-solutions/js/hw-5/task-3.js
@@ -3,8 +3,25 @@
  * Переменная `word` уже создана и содержит строку со словом.
  * Ожидаемый результат для `hello`: "hello contains 2 vowels and 3 consonants".
  */
+
 const word = 'hello';
 
-let vowelsAndConsonantsResult = '';
+const vowels = 'aeiouAEIOU';
+
+let vowelsCount = 0;
+let consonantsCount = 0;
+
+for (let i = 0; i < word.length; i++) {
+    let letter = word[i].toLowerCase()
+
+if (vowels.includes(letter)) {
+    vowelsCount++
+} else if (letter >= 'a' && letter <= 'z') {
+    consonantsCount++;
+}
+}
+ 
+let vowelsAndConsonantsResult = `hello contains ${vowelsCount} vowels and ${consonantsCount} consonants`;
+console.log(vowelsAndConsonantsResult)
 
 export { vowelsAndConsonantsResult };
